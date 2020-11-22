82e3d419a73065b6e5933ad2a3d08183c75e4ba3
Mercedez Harrison <mharriso@at-j5.msk.21-school.ru>
2020-11-22 20:44:06 +0300

    upd

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mharriso <mharriso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 00:31:51 by mharriso          #+#    #+#             */
-/*   Updated: 2020/11/21 23:19:14 by mharriso         ###   ########.fr       */
+/*   Updated: 2020/11/22 19:25:49 by mharriso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int cut_next_line(char *buffer, char *new_line, char *cache)
 {
 	return 0;
 }
-int	save_next_line(char *buffer, char **line, char *cache)
+
+int	save_next_line(char *buffer, char **line)
 {
 	char	*new_line;
 	char	*update_line;
@@ -27,8 +28,8 @@ int	save_next_line(char *buffer, char **line, char *cache)
 		return (-1);
 	free(*line);
 	*line = update_line;
-	if(new_line)
-		cut_next_line(buffer, new_line, cache);
+	//if(new_line)
+		//cut_next_line(buffer, new_line, cache);
 	return (new_line) ? 1 : 0;
 }
 
@@ -38,17 +39,21 @@ int	get_next_line(int fd, char **line)
 	char		buffer[BUFFER_SIZE + 1];
 	int			res;
 
-	if(!(*line = malloc(sizeof(char))))
+	if(!(*line = ft_strdup("")))
 		return (-1);
-	**line = '\0';
+
+
 	while((res = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[res] = '\0';
-		res = save_next_line(buffer, line);
+		res = save_next_line(buffer, line, cache);
 		if(res != 0)
 			break ;
 	}
 	if(res == -1)
+	{
+		free(cache);
 		free(*line);
+	}
 	return (res);
 }
