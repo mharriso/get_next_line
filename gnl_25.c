557de4c8b571821eacd8d2cabdfd0bc52d7deca7
Julia <63446729+mharriso@users.noreply.github.com>
2020-12-16 22:05:55 +0300

    without utils

--- gnl_25.c
+++ gnl_25.c
@@ -1,21 +1,33 @@
-
-int	get_next_line(int fd, char **line)
+static	char	*ft_joinchr(char const *s1, char c, size_t len)
 {
-	char		buffer[BUFFER_SIZE + 1];
+	char		*s;
+	size_t		i;
+
+	if (!(s = (char*)malloc((len + 1) * sizeof(char))))
+		return (NULL);
+	i = 0;
+	while (i < len)
+		s[i++] = (*s1) ? *s1++ : c;
+	s[i] = '\0';
+	return (s);
+}
+int		get_next_line(int fd, char **line)
+{
+	char		buffer;
 	int			res;
 	char		*tmp;
+	size_t		len;
 
 	if (fd < 0 || BUFFER_SIZE != 1 || !line || !(*line = (char *)malloc(1)))
 		return (-1);
 	**line = '\0';
-	while ((res = read(fd, buffer, BUFFER_SIZE)) > 0)
+	len = 0;
+	while ((res = read(fd, &buffer, BUFFER_SIZE)) > 0 && buffer != '\n')
 	{
-		buffer[res] = '\0';
-		if (*buffer == '\n')
-			return (1);
-		res = 0;
-		if (!(tmp = ft_strjoin(*line, buffer)))
+		len++;
+		if (!(tmp = ft_joinchr(*line, buffer, len)))
 			res = -1;
+		res = 0;
 		free(*line);
 		*line = tmp;
 		if (res != 0)
